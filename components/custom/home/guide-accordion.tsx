8d9611931023f24bc3b5e5cd8ed2d44cf1dfdac8
Christian Gonzales <chrisgonzales.online@gmail.com>
2024-02-05 02:11:40 +0800

    Refactor accordion guide

--- components/custom/home/guide-accordion.tsx
+++ components/custom/home/guide-accordion.tsx
@@ -5,50 +5,47 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+const guides: { value: string; triggerText: string; content: string }[] = [
+  {
+    value: "calendar",
+    triggerText: "Calendar",
+    content:
+      "The Calendar tab is your scheduling hub. Coaches, block your 2-hour schedules to make them available for students to book. Students, explore and book available slots set by your coach. Manage your time efficiently with our easy-to-use scheduling system.",
+  },
+  {
+    value: "upcoming",
+    triggerText: "Upcoming",
+    content:
+      "The Upcoming tab keeps you informed about your upcoming appointments. Coaches, cancel appointments to unblock the schedule, and view your students' details. Students, cancel your appointments or see details about your booked slots. Manage your commitments hassle-free.",
+  },
+  {
+    value: "history",
+    triggerText: "History",
+    content:
+      "The History tab provides a comprehensive view of your past appointments. Coaches, see details of your historical appointments with students and add scores and notes for your reference. Students, revisit your past appointments and review the details of your interactions with your coach. Reflect on your journey together.",
+  },
+  {
+    value: "switch",
+    triggerText: "Switch User",
+    content:
+      "The User Switching feature allows you to seamlessly transition between coach and student profiles. Locate the user selector dropdown in the footer to toggle between user types. Enhance your experience by easily exploring the functionalities specific to each user role.",
+  },
+];
+
 const GuideAccordion = () => {
   return (
     <div className="flex flex-col gap-6">
       <span className="text-3xl md:text-5xl">quickguide</span>
       <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="calendar">
-          <AccordionTrigger>Calendar</AccordionTrigger>
-          <AccordionContent>
-            The Calendar tab is your scheduling hub. Coaches, block your 2-hour
-            schedules to make them available for students to book. Students,
-            explore and book available slots set by your coach. Manage your time
-            efficiently with our easy-to-use scheduling system.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="upcoming">
-          <AccordionTrigger>Upcoming</AccordionTrigger>
-          <AccordionContent>
-            The Upcoming tab keeps you informed about your upcoming
-            appointments. Coaches, cancel appointments to unblock the schedule,
-            and view your students&apos; details. Students, cancel your
-            appointments or see details about your booked slots. Manage your
-            commitments hassle-free.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="history">
-          <AccordionTrigger>History</AccordionTrigger>
-          <AccordionContent>
-            The History tab provides a comprehensive view of your past
-            appointments. Coaches, see details of your historical appointments
-            with students and add scores and notes for your reference. Students,
-            revisit your past appointments and review the details of your
-            interactions with your coach. Reflect on your journey together.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="switch">
-          <AccordionTrigger>Switch User</AccordionTrigger>
-          <AccordionContent>
-            The User Switching feature allows you to seamlessly transition
-            between coach and student profiles. Locate the user selector
-            dropdown in the footer to toggle between user types. Enhance your
-            experience by easily exploring the functionalities specific to each
-            user role.
-          </AccordionContent>
-        </AccordionItem>
+        {guides.map((guide) => (
+          <AccordionItem
+            key={`accordion-guide-${guide.value}`}
+            value={guide.value}
+          >
+            <AccordionTrigger>{guide.triggerText}</AccordionTrigger>
+            <AccordionContent>{guide.content}</AccordionContent>
+          </AccordionItem>
+        ))}
       </Accordion>
     </div>
   );
