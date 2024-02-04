9edc6fe895ddce2983d31fed237299bba3c933e4
Christian Gonzales <chrisgonzales.online@gmail.com>
2024-02-04 18:17:08 +0800

    Create footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/custom/header/header";
+import { Footer } from "@/components/custom/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} min-h-screen flex flex-col bg-neutral-50`}
+      >
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/custom/footer/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="h-20 w-full bg-neutral-200 flex items-center justify-center">
+        Footer here
+      </div>
+    </footer>
+  );
+};
+
+export { Footer };
